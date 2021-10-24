a610994a49b3ddefc1b32dbdee5c88942bbfeadd
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2021-10-24 17:46:58 -0400

    Update 1-Live.sh

--- HPDesktop/1-Live.sh
+++ HPDesktop/1-Live.sh
@@ -53,48 +53,12 @@ mount ${DISK}1 /mnt/boot/efi
 (
 echo Y
 ) | xbps-install -Sy -R https://repo-us.voidlinux.org/current/musl -r /mnt base-system cryptsetup grub-x86_64-efi lvm2
-chroot /mnt
-chown root:root /
-chmod 755 /
-echo "Please create new root password:"
-read ROOT # stores the user's input which will be called on by ${ROOT}
-echo -e "${ROOT}\n${ROOT}" | passwd root # setting the root password
-echo ${NAME} > /etc/hostname
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-echo "LC_COLLATE=C" >> /etc/locale.conf
-
-echo "-------------------------------------------------"
-echo "-----       Filesystem configuration        -----"
-echo "-------------------------------------------------"
-echo "/dev/${NAME}/root  /         xfs      defaults              0       0" >> /etc/fstab
-echo "/dev/${NAME}/home  /home     xfs      defaults              0       0" >> /etc/fstab
-echo "${DISK}1         /boot/efi vfat     defaults              0       0" >> /etc/fstab
-
-echo "-------------------------------------------------"
-echo "-----          GRUB configuration           -----"
-echo "-------------------------------------------------"
-echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
-blkid -o value -s UUID ${DISK}2
-read UUID
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=rd.lvm.vg=${NAME} rd.luks.uuid=${UUID}/' /etc/default/grub
-
-echo "-------------------------------------------------"
-echo "-----            LUKS key setup             -----"
-echo "-------------------------------------------------"
-dd bs=1 count=64 if=/dev/urandom of=/boot/volume.key
-(
-echo ${PASS} 
-) | cryptsetup luksAddKey ${DISK}2 /boot/volume.key
-chmod 000 /boot/volume.key
-chmod -R g-rwx,o-rwx /boot
-echo "${NAME}   ${DISK}2   /boot/volume.key   luks" >> /etc/crypttab
-echo 'install_items+=" /boot/volume.key /etc/crypttab "' > /etc/dracut.conf.d/10-crypt.conf
-
-echo "-------------------------------------------------"
-echo "-----     Complete system installation      -----"
-echo "-------------------------------------------------"
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Void" --removable
-xbps-reconfigure -fa
-exit 
+curl -O https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/HPDesktop/1-LivePart2.sh
+mv 1-LivePart2.sh /mnt
+chroot /mnt sh ./1-LivePart2.sh
+rm /mnt/1-LivePart2.sh
 umount -R /mnt
-shutdown now
+
+echo "-------------------------------------------------"
+echo "-----   You may now shut down the system    -----"
+echo "-------------------------------------------------"
