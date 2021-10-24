598855a68419072499236cc82c7f0db10274d0b3
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2021-10-24 12:15:34 -0400

    Update 1-Live.sh

--- HPDesktop/1-Live.sh
+++ HPDesktop/1-Live.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 echo "-------------------------------------------------"
 echo "-----             Partitioning              -----"
 echo "-------------------------------------------------"
@@ -11,15 +9,12 @@ echo g # creates a new empty GPT partition table (clears out any partitions on t
 echo n # adds a new partition
 echo 1 # sets the new primary partition as the first partition on the drive
 echo   # accepts default value for first sector
-echo +260M # specifies last sector as 260M from first sector
+echo +128M # specifies last sector as 260M from first sector
 echo t # changes a partition type
 echo 1 # specifies what partition type partition 1 is being changed to (1 = EFI System)
 echo n # adds a new partition
 echo 2 # sets the new primary partition as the second partition on the drive
 echo   # accepts default value for first sector
 echo   # accepts default value for last sector
-echo t # changes a partition type
-echo 2 # selecting which partition's type will be changed
-echo 23 # # specifies what partition type partition 2 is being changed to (23 = Linux x86-64 root)
 echo w # writes partition table to disk
 ) | fdisk ${DISK}
