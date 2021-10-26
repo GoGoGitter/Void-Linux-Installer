57e413360b99f08f902aff3980aa2f5f97f3f5e8
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2021-10-26 19:39:26 -0400

    Update 1-EncryptedLivePart2.sh

--- HPDesktop/1-EncryptedLivePart2.sh
+++ HPDesktop/1-EncryptedLivePart2.sh
@@ -18,9 +18,9 @@ echo "LC_COLLATE=C" >> /etc/locale.conf
 echo "-------------------------------------------------"
 echo "-----       Filesystem configuration        -----"
 echo "-------------------------------------------------"
-echo "/dev/${NAME}/root  /         ext4     defaults              0       0" >> /etc/fstab
-echo "/dev/${NAME}/home  /home     ext4     defaults              0       0" >> /etc/fstab
-echo "${DISK}1         /boot/efi vfat     defaults              0       0" >> /etc/fstab
+echo "/dev/${NAME}/root / ext4 defaults 0 1" >> /etc/fstab
+echo "/dev/${NAME}/home /home ext4 defaults 0 2" >> /etc/fstab
+echo "${DISK}1 /boot/efi vfat defaults 0 2" >> /etc/fstab
 
 echo "-------------------------------------------------"
 echo "-----          GRUB configuration           -----"
