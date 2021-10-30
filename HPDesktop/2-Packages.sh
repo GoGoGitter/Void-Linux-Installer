1c88538e399a6d286eda5e51141caaca27a9e744
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2021-10-30 19:21:08 -0400

    Update 2-Packages.sh

--- HPDesktop/2-Packages.sh
+++ HPDesktop/2-Packages.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-SSL_NO_VERIFY_PEER=true xbps-install -y void-repo-nonfree # Void has a nonfree repository for packages that don't have free licenses. It can enabled by installing the void-repo-nonfree package.
+echo "-------------------------------------------------"
+echo "-----              More Stuff               -----"
+echo "-------------------------------------------------"
+touch /etc/xbps.d/settings.conf
+echo "architecture=x86_64-musl" >> /etc/xbps.d/settings.conf
+echo "repository=https://repo-us.voidlinux.org/current/musl" >> /etc/xbps.d/settings.conf
+echo "repository=https://repo-us.voidlinux.org/current/musl/nonfree" >> /etc/xbps.d/settings.conf
+
 SSL_NO_VERIFY_PEER=true xbps-install -Su # XBPS must use a separate transaction to update itself.
 SSL_NO_VERIFY_PEER=true xbps-install -Su # If your update includes the xbps package, you will need to run the command a second time to apply the rest of the updates.
 
 ### for intel microcode
+#SSL_NO_VERIFY_PEER=true xbps-install -y void-repo-nonfree # Void has a nonfree repository for packages that don't have free licenses. It can enabled by installing the void-repo-nonfree package.
 #SSL_NO_VERIFY_PEER=true xbps-install -y intel-ucode # After installing this package, it is necessary to regenerate your initramfs.
 #xbps-reconfigure --force linux<x>.<y> # For subsequent updates, the microcode will be added to the initramfs automatically.
 
