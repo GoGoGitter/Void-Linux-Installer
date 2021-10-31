8f8ea55d6ca61b9fd17fde96ff4cd40026baa024
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2021-10-31 00:26:25 -0400

    Update 1-Live.sh

--- HPDesktop/1-Live.sh
+++ HPDesktop/1-Live.sh
@@ -55,7 +55,7 @@ mkdir -p /mnt/boot/efi
 mount ${DISK}1 /mnt/boot/efi
 (
 echo Y # piping the answer to a question about importing keys because the -y flag does not deal with it 
-) | SSL_NO_VERIFY_PEER=true xbps-install -Sy -r /mnt base-system cryptsetup grub-x86_64-efi lvm2
+) | SSL_NO_VERIFY_PEER=true xbps-install -Sy -r /mnt base-system cryptsetup grub-x86_64-efi lvm2 doas
 curl -k -O https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/HPDesktop/1-LivePart2.sh
 mv 1-LivePart2.sh /mnt
 chroot /mnt /bin/bash ./1-LivePart2.sh
