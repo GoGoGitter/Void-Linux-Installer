b8e0726aa1699fae49e0c6eacdc028ff1b3b749c
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2021-10-31 00:29:42 -0400

    Update 1-Live.sh

--- HPDesktop/1-Live.sh
+++ HPDesktop/1-Live.sh
@@ -6,6 +6,7 @@ echo "-------------------------------------------------"
 touch /etc/xbps.d/settings.conf # making a config file for XBPS
 echo "architecture=x86_64-musl" >> /etc/xbps.d/settings.conf
 echo "repository=https://repo-us.voidlinux.org/current/musl" >> /etc/xbps.d/settings.conf
+echo "ignorepkg=sudo" >> /etc/xbps.d/settings.conf
 SSL_NO_VERIFY_PEER=true xbps-install -Su
 SSL_NO_VERIFY_PEER=true xbps-install -Su
 
