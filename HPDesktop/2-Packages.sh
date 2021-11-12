03fc086a453c5e86e9c5e7c53c4f1a4f437b58bb
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2021-11-12 11:59:27 -0500

    Update 2-Packages.sh

--- HPDesktop/2-Packages.sh
+++ HPDesktop/2-Packages.sh
@@ -9,14 +9,14 @@ echo "repository=https://repo-us.voidlinux.org/current/musl" >> /etc/xbps.d/sett
 echo "repository=https://repo-us.voidlinux.org/current/musl/nonfree" >> /etc/xbps.d/settings.conf
 echo "ignorepkg=sudo" >> /etc/xbps.d/settings.conf
 
-xbps-install -Su # XBPS must use a separate transaction to update itself.
-xbps-install -Su # If your update includes the xbps package, you will need to run the command a second time to apply the rest of the updates.
+xbps-install -Suy # XBPS must use a separate transaction to update itself.
+xbps-install -Suy # If your update includes the xbps package, you will need to run the command a second time to apply the rest of the updates.
+xbps-install -Sy void-repo-nonfree
 
 ### for intel microcode
-#SSL_NO_VERIFY_PEER=true xbps-install -y void-repo-nonfree # Void has a nonfree repository for packages that don't have free licenses. It can enabled by installing the void-repo-nonfree package.
-#SSL_NO_VERIFY_PEER=true xbps-install -y intel-ucode # After installing this package, it is necessary to regenerate your initramfs.
-#VER=$(echo $(uname -r) | sed 's/\./ /2' | sed 's/ \w*$//')
+#xbps-install -Sy intel-ucode # After installing this package, it is necessary to regenerate your initramfs.
+#VER=$(echo $(uname -r) | sed 's/\./ /2' | sed 's/ \w*$//') # uname -r outputs in the form x.y.z_a. This alters the string to the form x.y for the following command
 #xbps-reconfigure --force linux${VER} # For subsequent updates, the microcode will be added to the initramfs automatically.
 
 ### for amd microcode
-#SSL_NO_VERIFY_PEER=true xbps-install -y linux-firmware-amd # AMD CPUs and GPUs will automatically load the microcode, no further configuration required.
+#xbps-install -Sy linux-firmware-amd
