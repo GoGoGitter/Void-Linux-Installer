6a6e1a809611120576d2079bf593e458ff61a0f7
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2021-11-12 12:23:21 -0500

    Update 2-Packages.sh

--- HPDesktop/2-Packages.sh
+++ HPDesktop/2-Packages.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
 echo "-------------------------------------------------"
-echo "-----              More Stuff               -----"
+echo "-----          XBPS configuration          -----"
 echo "-------------------------------------------------"
 touch /etc/xbps.d/settings.conf
 echo "architecture=x86_64-musl" >> /etc/xbps.d/settings.conf
 echo "repository=https://repo-us.voidlinux.org/current/musl" >> /etc/xbps.d/settings.conf
 echo "repository=https://repo-us.voidlinux.org/current/musl/nonfree" >> /etc/xbps.d/settings.conf
 echo "ignorepkg=sudo" >> /etc/xbps.d/settings.conf
-
 xbps-install -Suy # XBPS must use a separate transaction to update itself.
 xbps-install -Suy # If your update includes the xbps package, you will need to run the command a second time to apply the rest of the updates.
 xbps-install -Sy void-repo-nonfree
+
+echo "-------------------------------------------------"
+echo "-----               Microcode               -----"
+echo "-------------------------------------------------"
 
 ### for intel microcode
 #xbps-install -Sy intel-ucode # After installing this package, it is necessary to regenerate your initramfs.
