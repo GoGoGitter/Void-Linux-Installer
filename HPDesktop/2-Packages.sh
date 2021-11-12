d86f51a1ff31b14dd0830d1e72062539f2e7d3c3
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2021-11-12 14:27:39 -0500

    Update 2-Packages.sh

--- HPDesktop/2-Packages.sh
+++ HPDesktop/2-Packages.sh
@@ -3,29 +3,39 @@
 echo "-------------------------------------------------"
 echo "-----          XBPS configuration          -----"
 echo "-------------------------------------------------"
-touch /etc/xbps.d/settings.conf
-echo "architecture=x86_64-musl" >> /etc/xbps.d/settings.conf
-echo "repository=https://repo-us.voidlinux.org/current/musl" >> /etc/xbps.d/settings.conf
-echo "repository=https://repo-us.voidlinux.org/current/musl/nonfree" >> /etc/xbps.d/settings.conf
-echo "ignorepkg=sudo" >> /etc/xbps.d/settings.conf
-xbps-install -Suy # XBPS must use a separate transaction to update itself.
-xbps-install -Suy # If your update includes the xbps package, you will need to run the command a second time to apply the rest of the updates.
-xbps-install -Sy void-repo-nonfree
+doas touch /etc/xbps.d/settings.conf
+doas sh -c 'echo "architecture=x86_64-musl" >> /etc/xbps.d/settings.conf'
+doas sh -c 'echo "repository=https://repo-us.voidlinux.org/current/musl" >> /etc/xbps.d/settings.conf'
+doas sh -c 'echo "repository=https://repo-us.voidlinux.org/current/musl/nonfree" >> /etc/xbps.d/settings.conf'
+doas sh -c 'echo "ignorepkg=sudo" >> /etc/xbps.d/settings.conf'
+doas xbps-install -Suy # XBPS must use a separate transaction to update itself.
+doas xbps-install -Suy # If your update includes the xbps package, you will need to run the command a second time to apply the rest of the updates.
+doas xbps-install -Sy void-repo-nonfree
 
 echo "-------------------------------------------------"
 echo "-----               Microcode               -----"
 echo "-------------------------------------------------"
 
 ### for intel microcode
-#xbps-install -Sy intel-ucode # After installing this package, it is necessary to regenerate your initramfs.
+#doas xbps-install -Sy intel-ucode # After installing this package, it is necessary to regenerate your initramfs.
 #VER=$(echo $(uname -r) | sed 's/\./ /2' | sed 's/ \w*$//') # uname -r outputs in the form x.y.z_a. This alters the string to the form x.y for the following command
-#xbps-reconfigure --force linux${VER} # For subsequent updates, the microcode will be added to the initramfs automatically.
+#doas xbps-reconfigure --force linux${VER} # For subsequent updates, the microcode will be added to the initramfs automatically.
 
 ### for amd microcode
-#xbps-install -Sy linux-firmware-amd
+#doas xbps-install -Sy linux-firmware-amd
 
 echo "-------------------------------------------------"
 echo "-----                  NTP                  -----"
 echo "-------------------------------------------------"
-xbps-install -Sy chrony
-ln -s /etc/sv/chronyd /var/service/
+doas xbps-install -Sy chrony
+doas ln -s /etc/sv/chronyd /var/service/
+
+echo "-------------------------------------------------"
+echo "-----                 Cron                  -----"
+echo "-------------------------------------------------"
+doas xbps-install -Sy dcron
+doas ln -s /etc/sv/dcron /var/service/
+doas crontab -l > tmp.txt
+echo "@monthly ID=remove-old-kernels vkpurge rm all" >> tmp.txt
+cat tmp.txt | doas crontab -
+rm tmp.txt
