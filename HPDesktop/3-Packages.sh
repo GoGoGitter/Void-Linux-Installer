8a83d4633442df9308c86d629b9b8ae73f1c80a7
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2021-11-16 17:57:33 -0500

    Update 3-Packages.sh

--- HPDesktop/3-Packages.sh
+++ HPDesktop/3-Packages.sh
@@ -1,9 +1,9 @@
 # Packages
+doas xbps-install -Sy ffmpeg # dependency for a git package: ani-cli
 
 # Source Packages
 
 # Git Clones
-doas xbps-install -Sy ffmpeg
 cd ~/.git-clones
 git clone https://github.com/pystardust/ani-cli.git
 cd ani-cli
