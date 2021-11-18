d5d561e69afb04388419c73670b831abee0967fc
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2021-11-17 23:23:03 -0500

    Update 3-Packages.sh

--- HPDesktop/3-Packages.sh
+++ HPDesktop/3-Packages.sh
@@ -1,10 +1,53 @@
-# Packages
-doas xbps-install -Sy mpv
-doas xbps-install -Sy ffmpeg # dependency for a git package: ani-cli
+### Packages
+xdg-user-dirs # installing general user directories
+light
+pulsemixer
+#DNSSEC
+xf86-input-mtrack
+#alternative to locate
+font-hack-ttf
+#normal font
+#fontpreviewer
+papirus-icon-theme
+arc-theme
+#'firefox'               # Browser
+#'nnn'                   # File Manager
+#'trash-cli'             # Trash Utility
+#'udisks2'               # Auto-mounting
+#'udiskie'               # Auto-mounting
+#'ytop'                  # System Monitor
+#'dunst'                 # Notification Manager
+#'parcellite'            # Clipboard Manager
+#'xclip'                 # Clipboard Utility (keepass-cli requires it for copying password entries)
+#'redshift'              # Blue Light Filter
+#'gnome-screenshot'      # Screenshot Utility
+#'sxiv'                  # Image Viewer
+#'mpv'                   # Multimedia Player
+#'yt-dlp'                # Youtube-dl fork
+#'p7zip'                 # Archiving and Extracting Tool
+#'transmission'          # BitTorrent Client
+#'keepassxc'             # Password Manager
+#'bleachbit'             # Disk Cleaning Utility
+#'tor'                   # Tor
+#'libreoffice'           # Office Suite
+#'hunspell'              # Office Suite
+#'hunspell-en_US'        # Office Suite
+#'hyphen'                # Office Suite
+#'mythes'                # Office Suite
+#'evtest'                # Gamepad Support
+#'nitrogen'              # Wallpaper Setter
+#'openssh'               # OpenBSD Secure Shell
+#'ffmpeg'                # dependency for a git package: ani-cli
 
-# Source Packages
+### Source Packages
+#'dwm'                # Window Manager
+#'st'                 # Terminal Emulator
+#'dmenu'              # Application Launcher
+#'slock'              # Screen Locker
+#'slstatus'           # Status Monitor
+#'surf'               # Browser
 
-# Git Clones
+### Git Clones
 cd ~/.git-clones
 git clone https://github.com/pystardust/ani-cli.git
 cd ani-cli
