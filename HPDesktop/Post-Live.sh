b1a18816313bd517dded40f4613e328686485eb9
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2021-11-25 20:27:26 -0500

    Update Post-Live.sh

--- HPDesktop/Post-Live.sh
+++ HPDesktop/Post-Live.sh
@@ -7,3 +7,7 @@ rm 2-Configuration.sh
 curl -O https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/HPDesktop/3-Packages.sh
 /bin/bash 3-Packages.sh
 rm 3-Packages.sh
+
+curl -O https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/HPDesktop/4-Dotfiles.sh
+/bin/bash 4-Dotfiles.sh
+rm 4-Dotfiles.sh
