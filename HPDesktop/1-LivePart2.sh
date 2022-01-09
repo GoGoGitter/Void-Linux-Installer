8bdee005cb0713fe63bf755db5900b62a6810970
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2022-01-09 13:26:52 -0500

    Update 1-LivePart2.sh

--- HPDesktop/1-LivePart2.sh
+++ HPDesktop/1-LivePart2.sh
@@ -28,15 +28,15 @@ echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
 UUID=$(blkid -o value -s UUID ${DISK}2)
 sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT='rd.lvm.vg=$HOST rd.luks.uuid=$UUID'/" /etc/default/grub
 
-echo "-------------------------------------------------"
-echo "-----            LUKS key setup             -----"
-echo "-------------------------------------------------"
-dd bs=1 count=64 if=/dev/urandom of=/boot/volume.key
-cryptsetup luksAddKey ${DISK}2 /boot/volume.key
-chmod 000 /boot/volume.key
-chmod -R g-rwx,o-rwx /boot
-echo "${HOST}   ${DISK}2   /boot/volume.key   luks" >> /etc/crypttab
-echo 'install_items+=" /boot/volume.key /etc/crypttab "' > /etc/dracut.conf.d/10-crypt.conf
+#echo "-------------------------------------------------"
+#echo "-----            LUKS key setup             -----"
+#echo "-------------------------------------------------"
+#dd bs=1 count=64 if=/dev/urandom of=/boot/volume.key
+#cryptsetup luksAddKey ${DISK}2 /boot/volume.key
+#chmod 000 /boot/volume.key
+#chmod -R g-rwx,o-rwx /boot
+#echo "${HOST}   ${DISK}2   /boot/volume.key   luks" >> /etc/crypttab
+#echo 'install_items+=" /boot/volume.key /etc/crypttab "' > /etc/dracut.conf.d/10-crypt.conf
 
 echo "-------------------------------------------------"
 echo "-----     Complete system installation      -----"
