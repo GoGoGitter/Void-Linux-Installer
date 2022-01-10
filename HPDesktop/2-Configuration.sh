db8b158d3ac98696a042015668c26c2cf12dae80
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2022-01-10 13:37:38 -0500

    Update 2-Configuration.sh

--- HPDesktop/2-Configuration.sh
+++ HPDesktop/2-Configuration.sh
@@ -44,7 +44,8 @@ echo "-------------------------------------------------"
 doas sed -i 's|/boot/efi vfat defaults|/boot/efi vfat defaults,discard|' /etc/fstab
 doas sed -i 's/issue_discards.*/issue_discards=1/' /etc/lvm/lvm.conf
 grep GRUB_CMDLINE_LINUX_DEFAULT.* /etc/default/grub > tmp.txt
-sed "s/'\([^']*\)$/ rd.luks.allow-discards'\1/" tmp.txt
+DISC=$(sed "s/'\([^']*\)$/ rd.luks.allow-discards'\1/" tmp.txt)
+sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=.*/$DISC/" /etc/default/grub
 
 #echo "-------------------------------------------------"
 #echo "-----               Security                -----"
