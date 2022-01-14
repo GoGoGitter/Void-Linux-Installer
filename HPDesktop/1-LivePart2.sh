bb877046ad50a3ffadc37971d6a0a8a4fff83965
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2022-01-13 19:28:10 -0500

    Update 1-LivePart2.sh

--- HPDesktop/1-LivePart2.sh
+++ HPDesktop/1-LivePart2.sh
@@ -1,47 +1,40 @@
 #!/usr/bin/env bash
 
-chown root:root /
-chmod 755 /
+echo "-------------------------------------------------"
+echo "-----      Installation Configuration       -----"
+echo "-------------------------------------------------"
+echo "Please enter a hostname:"
+read HOST
+echo ${HOST} > /etc/hostname
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+echo "-------------------------------------------------"
+echo "-----          Set a Root Password          -----"
+echo "-------------------------------------------------"
 echo "Please create new root password:"
 read ROOT # stores the user's input which will be called on by ${ROOT}
 (
 echo ${ROOT}
 echo ${ROOT}
 ) | passwd root
-echo ${HOST} > /etc/hostname
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-echo "en_US.UTF-8 UTF-8" >> /etc/default/libc-locales
-xbps-reconfigure -f glibc-locales
 
 echo "-------------------------------------------------"
-echo "-----       Filesystem configuration        -----"
+echo "-----            Configure fstab            -----"
 echo "-------------------------------------------------"
 echo "tmpfs /tmp tmpfs defaults,nosuid,nodev 0 0" > /etc/fstab
-echo "UUID=$(blkid -o value -s UUID /dev/${HOST}/root) / ext4 defaults 0 1" >> /etc/fstab
-echo "UUID=$(blkid -o value -s UUID /dev/${HOST}/home) /home ext4 defaults 0 2" >> /etc/fstab
+echo "UUID=$(blkid -o value -s UUID ${DISK}2) / ext4 defaults 0 1" >> /etc/fstab
+echo "UUID=$(blkid -o value -s UUID ${DISK}4) /home ext4 defaults 0 2" >> /etc/fstab
 echo "UUID=$(blkid -o value -s UUID ${DISK}1) /boot/efi vfat defaults 0 2" >> /etc/fstab
+echo "UUID=$(blkid -o value -s UUID ${DISK}3) swap swap defaults 0 0" >> /etc/fstab
 
 echo "-------------------------------------------------"
-echo "-----          GRUB configuration           -----"
-echo "-------------------------------------------------"
-echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
-UUID=$(blkid -o value -s UUID ${DISK}2)
-sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT='rd.lvm.vg=$HOST rd.luks.uuid=$UUID'/" /etc/default/grub
-
-echo "-------------------------------------------------"
-echo "-----            LUKS key setup             -----"
-echo "-------------------------------------------------"
-dd bs=1 count=64 if=/dev/urandom of=/boot/volume.key
-cryptsetup luksAddKey ${DISK}2 /boot/volume.key
-chmod 000 /boot/volume.key
-chmod -R g-rwx,o-rwx /boot
-echo "${HOST}   ${DISK}2   /boot/volume.key   luks" >> /etc/crypttab
-echo 'install_items+=" /boot/volume.key /etc/crypttab "' > /etc/dracut.conf.d/10-crypt.conf
-
-echo "-------------------------------------------------"
-echo "-----     Complete system installation      -----"
+echo "-----            Installing GRUB            -----"
 echo "-------------------------------------------------"
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Void" --removable
+
+echo "-------------------------------------------------"
+echo "-----             Finalization              -----"
+echo "-------------------------------------------------"
 xbps-reconfigure -fa
 
 echo "-------------------------------------------------"
