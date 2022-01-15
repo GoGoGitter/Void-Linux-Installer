c0c206de03b643ac6238dbba957318937b356523
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2022-01-15 18:31:28 -0500

    Update 3-Packages.sh

--- HPDesktop/3-Packages.sh
+++ HPDesktop/3-Packages.sh
@@ -12,15 +12,11 @@ PKGS=(
 'gvfs'                  # auto-mounting + trash Support for Thunar
 'thunar-archive-plugin' # frontend for archivers
 'xarchiver'             # desktop independent archive manager
-'htop'                  # System Monitor
 'yt-dlp'                # Youtube-dl fork
 #'keepassxc'             # Password Manager
 #'bleachbit'             # Disk Cleaning Utility
-#'libreoffice'           # Office Suite
-#'hunspell'              # Office Suite
-#'hunspell-en_US'        # Office Suite
-#'hyphen'                # Office Suite
-#'mythes'                # Office Suite
+'libreoffice'           # Office Suite
+'hunspell-en_US'        # Office Suite
 #'openssh'               # OpenBSD Secure Shell
 )
 for PKG in "${PKGS[@]}"; do
