36c2ee452cfa6ad7fb6edcc1a3215175dc2af4c7
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2022-02-27 23:11:35 -0500

    Update 1-Live.sh

--- General/1-Live.sh
+++ General/1-Live.sh
@@ -1,1 +1,22 @@
-boi
+#!/usr/bin/env bash
+
+##################################################
+######             Partitioning             ######
+################################################## 
+fdisk -l
+echo "Please enter disk: (example /dev/sda)"
+read DISK 
+(
+echo g # creates a new empty GPT partition table (clears out any partitions on the drive)
+echo n # adds a new partition
+echo 1 # sets the new primary partition as the first partition on the drive
+echo   # accepts default value for first sector
+echo +256M # specifies last sector as 260M from first sector
+echo t # changes a partition's type (defaults to selecting partition 1 as it is the only existing one for now)
+echo 1 # specifies what partition type partition 1 is being changed to (1 = EFI System)
+echo n # adds a new partition
+echo 2 # sets the new primary partition as the second partition on the drive
+echo   # accepts default value for first sector
+echo   # accepts default value for last sector
+echo w # writes partition table to disk
+) | fdisk -W always ${DISK} # -W flag automatically wipes previously existing filesystem signatures upon writing the new partition table
