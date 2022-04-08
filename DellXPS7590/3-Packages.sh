51202d2f73cf5aa1d15238a4758f61f221849a44
GoGoGitter <joshuav320@hotmail.com>
2022-04-07 22:51:15 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   DellXPS7590/3-Packages.sh
    	modified:   DellXPS7590/4-Dotfiles.sh

--- DellXPS7590/3-Packages.sh
+++ DellXPS7590/3-Packages.sh
@@ -4,56 +4,56 @@ echo "-------------------------------------------------"
 echo "-----               Packages                -----"
 echo "-------------------------------------------------"
 PKGS=(
-'xdg-user-dirs'         # general user directories
+#'xdg-user-dirs'         # general user directories
 'light'                 # Manages Screen Brightness
 'rtkit'                 # 
 'pulsemixer'            # TUI app for controlling volume of applications
 ##DNSSEC                 # might use unbound
 #'xf86-input-mtrack'     # Driver for Touchpads
 ##alternative to locate  # might go with plocate
-'font-hack-ttf'         # monospace font
+#'font-hack-ttf'         # monospace font
 ##normal font            # general font (probably going with deja vu)
 ##fontpreviewer          # fontpreviewer
 'papirus-icon-theme'    # Icons
 'arc-theme'
 'firefox'               # Browser
 'chromium'              # Browser
-'nnn'                   # File Manager
-'xdotool'               # for nnn's preview-tabbed plugin
+#'nnn'                   # File Manager
+#'xdotool'               # for nnn's preview-tabbed plugin
 'trash-cli'             # Trash Utility
-'udisks2'               # Auto-mounting
-'udiskie'               # Auto-mounting
+#'udisks2'               # Auto-mounting
+#'udiskie'               # Auto-mounting
 'htop'                  # System Monitor
-'dunst'                 # Notification Manager
-'parcellite'            # Clipboard Manager
+#'dunst'                 # Notification Manager
+#'parcellite'            # Clipboard Manager
 'xclip'                 # Clipboard Utility (keepass-cli requires it for copying password entries)
-'redshift'              # Blue Light Filter
-'gnome-screenshot'      # Screenshot Utility
-'sxiv'                  # Image Viewer
-'mpv'                   # Multimedia Player
-'yt-dlp'                # Youtube-dl fork
-'p7zip'                 # Archiving and Extracting Tool
-'transmission'          # BitTorrent Client
+#'redshift'              # Blue Light Filter
+#'gnome-screenshot'      # Screenshot Utility
+#'sxiv'                  # Image Viewer
+#'mpv'                   # Multimedia Player
+#'yt-dlp'                # Youtube-dl fork
+#'p7zip'                 # Archiving and Extracting Tool
+#'transmission'          # BitTorrent Client
 'keepassxc'             # Password Manager
-'bleachbit'             # Disk Cleaning Utility
-'tor'                   # Tor
-'libreoffice'           # Office Suite
-'hunspell-en_US'        # Office Suite
-'evtest'                # for use with xboxdrv
-'nitrogen'              # Wallpaper Setter
-'openssh'               # OpenBSD Secure Shell
-'aria2'                 # dependency for a git package: ani-cli
-'jq'                    # dependency for a git package: ani-cli
-'xboxdrv'               # Xbox Gamepad Userspace Driver
-'protonvpn-cli'         # ProtonVPN
-'melonDS'               # Nintendo DS emulator
-'dolphin-emu'           # Gamecube/Wii emulator
-'steam'                 # Valve's steam client
-'libgcc-32bit'          # for steam
-'libstdc++-32bit'       # for steam
-'libdrm-32bit'          # for steam
-'libglvnd-32bit'        # for steam
-'mesa-dri-32bit'        # for steam
+#'bleachbit'             # Disk Cleaning Utility
+#'tor'                   # Tor
+#'libreoffice'           # Office Suite
+#'hunspell-en_US'        # Office Suite
+#'evtest'                # for use with xboxdrv
+#'nitrogen'              # Wallpaper Setter
+#'openssh'               # OpenBSD Secure Shell
+#'aria2'                 # dependency for a git package: ani-cli
+#'jq'                    # dependency for a git package: ani-cli
+#'xboxdrv'               # Xbox Gamepad Userspace Driver
+#'protonvpn-cli'         # ProtonVPN
+#'melonDS'               # Nintendo DS emulator
+#'dolphin-emu'           # Gamecube/Wii emulator
+#'steam'                 # Valve's steam client
+#'libgcc-32bit'          # for steam
+#'libstdc++-32bit'       # for steam
+#'libdrm-32bit'          # for steam
+#'libglvnd-32bit'        # for steam
+#'mesa-dri-32bit'        # for steam
 )
 for PKG in "${PKGS[@]}"; do
     doas xbps-install -Sy $PKG
@@ -61,11 +61,11 @@ done
 
 doas gpasswd -a ${USER} video # adding the user to the video group so that 'light' does not require root permission to work
 
-curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh # 'nnn' downloading nnn plugins
+#curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh # 'nnn' downloading nnn plugins
 
-doas gpasswd -a ${USER} input # 'evtest' adding user to input group to ensure gamepads can be used without root privileges
+#doas gpasswd -a ${USER} input # 'evtest' adding user to input group to ensure gamepads can be used without root privileges
 
-doas sh -c "echo 'blacklist xpad' > /etc/modprobe.d/xpad.conf"
+#doas sh -c "echo 'blacklist xpad' > /etc/modprobe.d/xpad.conf"
 
 #doas dracut --force --add-drivers bcm5974 # 'xf86-input-mtrack' without this, trackpad only moves up and down despite configuration given "because of some dracut driver inclusion issue" (from void wiki)
 
@@ -79,8 +79,8 @@ PKGS=(
 'st'                 # Terminal Emulator
 'dmenu'              # Application Launcher
 'slock'              # Screen Locker
-'slstatus'           # Status Monitor
-'tabbed'             # 
+#'slstatus'           # Status Monitor
+#'tabbed'             # 
 #                     # Minecraft Launcher
 )
 for PKG in "${PKGS[@]}"; do
@@ -90,10 +90,10 @@ done
 
 echo "exec dbus-run-session dwm" >> ~/.xinitrc
 
-echo "-------------------------------------------------"
-echo "-----             Git Packages              -----"
-echo "-------------------------------------------------"
-cd ~/.git-clones
-git clone https://github.com/pystardust/ani-cli.git
-cd ani-cli
-doas make
+#echo "-------------------------------------------------"
+#echo "-----             Git Packages              -----"
+#echo "-------------------------------------------------"
+#cd ~/.git-clones
+#git clone https://github.com/pystardust/ani-cli.git
+#cd ani-cli
+#doas make
--- DellXPS7590/4-Dotfiles.sh
+++ DellXPS7590/4-Dotfiles.sh
@@ -6,54 +6,54 @@ mkdir ~/.config
 #doas mkdir /etc/X11/xorg.conf.d/
 #doas sh -c 'curl -o /etc/X11/xorg.conf.d/50-synaptics.conf https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/mtrack'
 
-mkdir -p ~/.config/mpv/script-opts/
-curl -o ~/.config/mpv/mpv.conf https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/mpv
-curl -o ~/.config/mpv/input.conf https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/input.conf
-curl -o ~/.config/mpv/script-opts/osc.conf https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/osc.conf
+#mkdir -p ~/.config/mpv/script-opts/
+#curl -o ~/.config/mpv/mpv.conf https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/mpv
+#curl -o ~/.config/mpv/input.conf https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/input.conf
+#curl -o ~/.config/mpv/script-opts/osc.conf https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/osc.conf
 
 #doas sh -c 'curl -o /etc/modprobe.d/i915.conf https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/i915.conf'
 
 #mkdir ~/.config/rofi/
 #curl -o ~/.config/rofi/config.rasi https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/config.rasi
 
-mkdir ~/.config/dunst/
-curl -o ~/.config/dunst/dunstrc https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/dunstrc
+#mkdir ~/.config/dunst/
+#curl -o ~/.config/dunst/dunstrc https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/dunstrc
 
-mkdir ~/.config/redshift/
-curl -o ~/.config/redshift/redshift.conf https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/redshift.conf
+#mkdir ~/.config/redshift/
+#curl -o ~/.config/redshift/redshift.conf https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/redshift.conf
+#
+#mkdir -p ~/.config/sxiv/exec/
+#curl -o ~/.config/sxiv/exec/key-handler https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/key-handler
+#chmod a+x ~/.config/sxiv/exec/key-handler
+#
+#mkdir ~/.config/parcellite/
+#curl -o ~/.config/parcellite/parcelliterc https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/parcelliterc
+#
+#curl -o ~/.config/mimeapps.list https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/mimeapps.list
+#
+#mkdir ~/.config/xboxdrv/
+#curl -o ~/.config/xboxdrv/daemon.xboxdrv https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/daemon.xboxdrv
+#sed -i "s/USER/$USER/" ~/.config/xboxdrv/daemon.xboxdrv
+#curl -o ~/.config/xboxdrv/Xbox360-Nintendo.xboxdrv https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/Xbox360-Nintendo.xboxdrv
+#curl -o ~/.config/xboxdrv/all_options.xboxdrv https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/all_options.xboxdrv
+#curl -o ~/.config/xboxdrv/Red_M.xboxdrv https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/Red_M.xboxdrv
+#curl -o ~/.config/xboxdrv/Blue_Pro.xboxdrv https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/Blue_Pro.xboxdrv
+#curl -o ~/.config/xboxdrv/PS4.xboxdrv https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/PS4.xboxdrv
+#curl -o ~/.config/xboxdrv/Gamecube_Controller.xboxdrv https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/Gamecube_Controller.xboxdrv
+#curl -o ~/.config/xboxdrv/Joycon_Pair.xboxdrv https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/Joycon_Pair.xboxdrv
 
-mkdir -p ~/.config/sxiv/exec/
-curl -o ~/.config/sxiv/exec/key-handler https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/key-handler
-chmod a+x ~/.config/sxiv/exec/key-handler
+#curl -o ~/.bashrc https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/.bashrc
 
-mkdir ~/.config/parcellite/
-curl -o ~/.config/parcellite/parcelliterc https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/parcelliterc
-
-curl -o ~/.config/mimeapps.list https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/mimeapps.list
-
-mkdir ~/.config/xboxdrv/
-curl -o ~/.config/xboxdrv/daemon.xboxdrv https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/daemon.xboxdrv
-sed -i "s/USER/$USER/" ~/.config/xboxdrv/daemon.xboxdrv
-curl -o ~/.config/xboxdrv/Xbox360-Nintendo.xboxdrv https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/Xbox360-Nintendo.xboxdrv
-curl -o ~/.config/xboxdrv/all_options.xboxdrv https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/all_options.xboxdrv
-curl -o ~/.config/xboxdrv/Red_M.xboxdrv https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/Red_M.xboxdrv
-curl -o ~/.config/xboxdrv/Blue_Pro.xboxdrv https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/Blue_Pro.xboxdrv
-curl -o ~/.config/xboxdrv/PS4.xboxdrv https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/PS4.xboxdrv
-curl -o ~/.config/xboxdrv/Gamecube_Controller.xboxdrv https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/Gamecube_Controller.xboxdrv
-curl -o ~/.config/xboxdrv/Joycon_Pair.xboxdrv https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/Joycon_Pair.xboxdrv
-
-curl -o ~/.bashrc https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/.bashrc
-
-mkdir ~/.config/fontconfig/
-curl -o ~/.config/fontconfig/fonts.conf https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/fonts.conf
+#mkdir ~/.config/fontconfig/
+#curl -o ~/.config/fontconfig/fonts.conf https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/fonts.conf
 
 mkdir ~/.config/gtk-3.0/
 curl -o ~/.config/gtk-3.0/settings.ini https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/settings.ini
 
 curl -o ~/.vimrc https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/.vimrc
 
-mkdir ~/.config/udiskie/
-curl -o ~/.config/udiskie/config.yml https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/config.yml
+#mkdir ~/.config/udiskie/
+#curl -o ~/.config/udiskie/config.yml https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/config.yml
 
-mkdir ~/.ssh/
-curl -o ~/.ssh/config https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/ssh.config
+#mkdir ~/.ssh/
+#curl -o ~/.ssh/config https://raw.githubusercontent.com/GoGoGitter/Void-Linux-Installer/main/DellXPS7590/dotfiles/ssh.config
