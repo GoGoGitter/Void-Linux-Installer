edb7b9cfe97f7559cda87219e754465d016ca2f5
GoGoGitter <joshuav320@hotmail.com>
2022-04-07 22:00:24 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   DellXPS7590/1-Live.sh
    	new file:   DellXPS7590/1-LivePart2.sh
    	new file:   DellXPS7590/2-Configuration.sh
    	new file:   DellXPS7590/3-Packages.sh
    	new file:   DellXPS7590/4-Dotfiles.sh
    	new file:   DellXPS7590/Post-Live.sh
    	new file:   DellXPS7590/dotfiles/Blue_Pro.xboxdrv
    	new file:   DellXPS7590/dotfiles/Gamecube_Controller.xboxdrv
    	new file:   DellXPS7590/dotfiles/Joycon_Pair.xboxdrv
    	new file:   DellXPS7590/dotfiles/PS4.xboxdrv
    	new file:   DellXPS7590/dotfiles/Red_M.xboxdrv
    	new file:   DellXPS7590/dotfiles/Xbox360-Nintendo.xboxdrv
    	new file:   DellXPS7590/dotfiles/all_options.xboxdrv
    	new file:   DellXPS7590/dotfiles/bashrc
    	new file:   DellXPS7590/dotfiles/config.h
    	new file:   DellXPS7590/dotfiles/config.rasi
    	new file:   DellXPS7590/dotfiles/config.yml
    	new file:   DellXPS7590/dotfiles/daemon.xboxdrv
    	new file:   DellXPS7590/dotfiles/dunstrc
    	new file:   DellXPS7590/dotfiles/fonts.conf
    	new file:   DellXPS7590/dotfiles/i915.conf
    	new file:   DellXPS7590/dotfiles/input.conf
    	new file:   DellXPS7590/dotfiles/key-handler
    	new file:   DellXPS7590/dotfiles/mimeapps.list
    	new file:   DellXPS7590/dotfiles/mpv
    	new file:   DellXPS7590/dotfiles/mtrack
    	new file:   DellXPS7590/dotfiles/osc.conf
    	new file:   DellXPS7590/dotfiles/parcelliterc
    	new file:   DellXPS7590/dotfiles/picom.conf
    	new file:   DellXPS7590/dotfiles/redshift.conf
    	new file:   DellXPS7590/dotfiles/settings.ini
    	new file:   DellXPS7590/dotfiles/ssh.config
    	new file:   DellXPS7590/dotfiles/sxhkdrc
    	new file:   DellXPS7590/dotfiles/vimrc

---
+++ DellXPS7590/1-LivePart2.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+chown root:root /
+chmod 755 /
+echo "Please create new root password:"
+read ROOT # stores the user's input which will be called on by ${ROOT}
+(
+echo ${ROOT}
+echo ${ROOT}
+) | passwd root
+echo ${HOST} > /etc/hostname
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "en_US.UTF-8 UTF-8" >> /etc/default/libc-locales
+xbps-reconfigure -f glibc-locales
+
+echo "-------------------------------------------------"
+echo "-----       Filesystem configuration        -----"
+echo "-------------------------------------------------"
+echo "tmpfs /tmp tmpfs defaults,nosuid,nodev 0 0" > /etc/fstab
+echo "UUID=$(blkid -o value -s UUID /dev/${HOST}/root) / ext4 defaults 0 1" >> /etc/fstab
+echo "UUID=$(blkid -o value -s UUID /dev/${HOST}/home) /home ext4 defaults 0 2" >> /etc/fstab
+echo "UUID=$(blkid -o value -s UUID ${DISK}1) /boot/efi vfat defaults 0 2" >> /etc/fstab
+
+echo "-------------------------------------------------"
+echo "-----          GRUB configuration           -----"
+echo "-------------------------------------------------"
+echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
+UUID=$(blkid -o value -s UUID ${DISK}2)
+sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT='rd.lvm.vg=$HOST rd.luks.uuid=$UUID'/" /etc/default/grub
+
+echo "-------------------------------------------------"
+echo "-----            LUKS key setup             -----"
+echo "-------------------------------------------------"
+dd bs=1 count=64 if=/dev/urandom of=/boot/volume.key
+cryptsetup luksAddKey ${DISK}2 /boot/volume.key
+chmod 000 /boot/volume.key
+chmod -R g-rwx,o-rwx /boot
+echo "${HOST}   ${DISK}2   /boot/volume.key   luks" >> /etc/crypttab
+echo 'install_items+=" /boot/volume.key /etc/crypttab "' > /etc/dracut.conf.d/10-crypt.conf
+
+echo "-------------------------------------------------"
+echo "-----     Complete system installation      -----"
+echo "-------------------------------------------------"
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Void" --removable
+xbps-reconfigure -fa
+
+echo "-------------------------------------------------"
+echo "-----           Users and Groups            -----"
+echo "-------------------------------------------------"
+echo "Please enter a username:"
+read NAME
+useradd -m ${NAME}
+echo "Please enter a password for this user:"
+read PASS
+(
+echo ${PASS}
+echo ${PASS}
+) | passwd ${NAME}
+gpasswd -a ${NAME} wheel
+touch /etc/doas.conf
+echo "permit nopass :wheel as root" > /etc/doas.conf
+
+echo "-------------------------------------------------"
+echo "-----               Time zone               -----"
+echo "-------------------------------------------------"
+echo "Enter city:"
+read CITY
+ln -sf /usr/share/zoneinfo/America/${CITY} /etc/localtime
+hwclock --systohc
+
+echo "-------------------------------------------------"
+echo "-----               Network                 -----"
+echo "-------------------------------------------------"
+ln -s /etc/sv/dhcpcd /var/service/
+ln -s /etc/sv/dbus /var/service/
+ln -s /etc/sv/iwd /var/service/
