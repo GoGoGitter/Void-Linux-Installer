727ec91edaa24ff5b599f3cf106b20ba1a911023
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2022-05-13 14:32:14 -0400

    Update 1-LivePart2.sh

--- General/1-LivePart2.sh
+++ General/1-LivePart2.sh
@@ -36,8 +36,10 @@ dd bs=1 count=64 if=/dev/urandom of=/boot/volume.key
 cryptsetup luksAddKey ${ROOT_PART} /boot/volume.key --key-file temp-key.txt
 chmod 000 /boot/volume.key
 echo "${HOST}   ${ROOT_PART}   /boot/volume.key   luks" >> /etc/crypttab
-echo 'install_items+=" /boot/volume.key /etc/crypttab "' > /etc/dracut.conf.d/10-crypt.conf
-if [ "$DISK_NUM" = "2" ]
+if [ "$DISK_NUM" = "1" ]
+then
+  echo 'install_items+=" /boot/volume.key /etc/crypttab "' > /etc/dracut.conf.d/10-crypt.conf
+elif [ "$DISK_NUM" = "2" ]
 then
   dd bs=1 count=64 if=/dev/urandom of=/boot/volume2.key
   cryptsetup luksAddKey ${HOME_PART} /boot/volume2.key --key-file temp-key2.txt
