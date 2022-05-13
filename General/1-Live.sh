5ff0ab1638d5a9920061b62993ce98b4070a6356
GoGoGitter <81780046+GoGoGitter@users.noreply.github.com>
2022-05-13 00:53:45 -0400

    Update 1-Live.sh

--- General/1-Live.sh
+++ General/1-Live.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
 
+# Uncomment the Partitioning section if you wish to use a single drive and automate the partitioning.
 ##################################################
 ######             Partitioning             ######
 ################################################## 
-fdisk -l
-echo "Please enter disk: (example /dev/sda)"
-read DISK 
-(
-echo g # creates a new empty GPT partition table (clears out any partitions on the drive)
-echo n # adds a new partition
-echo 1 # sets the new primary partition as the first partition on the drive
-echo   # accepts default value for first sector
-echo +300M # specifies last sector as 260M from first sector
-echo t # changes a partition's type (defaults to selecting partition 1 as it is the only existing one for now)
-echo 1 # specifies what partition type partition 1 is being changed to (1 = EFI System)
-echo n # adds a new partition
-echo 2 # sets the new primary partition as the second partition on the drive
-echo   # accepts default value for first sector
-echo   # accepts default value for last sector
-echo w # writes partition table to disk
-) | fdisk -W always ${DISK} # -W flag automatically wipes previously existing filesystem signatures upon writing the new partition table
+#fdisk -l
+#echo "Please enter disk: (example /dev/sda)"
+#read DISK 
+#(
+#echo g # creates a new empty GPT partition table (clears out any partitions on the drive)
+#echo n # adds a new partition
+#echo 1 # sets the new primary partition as the first partition on the drive
+#echo   # accepts default value for first sector
+#echo +300M # specifies last sector as 300M from first sector
+#echo t # changes a partition's type (defaults to selecting partition 1 as it is the only existing one for now)
+#echo 1 # specifies what partition type partition 1 is being changed to (1 = EFI System)
+#echo n # adds a new partition
+#echo 2 # sets the new primary partition as the second partition on the drive
+#echo   # accepts default value for first sector
+#echo   # accepts default value for last sector
+#echo w # writes partition table to disk
+#) | fdisk -W always ${DISK} # -W flag automatically wipes previously existing filesystem signatures upon writing the new partition table
 
 echo "-------------------------------------------------"
 echo "-----    Encrypted volume configuration     -----"
